e756ce691714be3cf71d89b9fd32ddac0071fd61
Alex Vesier <alexvesier@gmail.com>
2020-12-16 14:46:26 +0100

    update

--- src/main/java/cesiDI19/groupe4/controller/OrganisationController.java
+++ src/main/java/cesiDI19/groupe4/controller/OrganisationController.java
@@ -6,29 +6,24 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import cesiDI19.groupe4.bean.Civil;
 import cesiDI19.groupe4.bean.Organisation;
-import cesiDI19.groupe4.services.CivilServices;
 import cesiDI19.groupe4.services.OrganisationServices;
 
 @Controller
-public class HomeController {
+public class OrganisationController {
 	
 	@Autowired
-	CivilServices civilServices;
 	OrganisationServices organisationServices;
 	
-	@RequestMapping("/home")
-    public String home(@RequestParam(required = false, defaultValue = "World")
+	@RequestMapping("/organisation")
+    public String organisation(@RequestParam(required = false, defaultValue = "World")
                            String name, Model model){
-		Civil civil = civilServices.getCivil();
-		model.addAttribute("civil",civil);
 		Organisation organisation = organisationServices.getOrganisation();
 		model.addAttribute("organisation",organisation);
 		
 		
 		
-        return "home";
+        return "organisation";
     }
 }
 
