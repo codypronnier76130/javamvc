097130d80404472ac0d7186d13454bc9e0c59e08
Louvel <noe.louvel@viacesi.fr>
2020-12-16 15:02:30 +0100

    Création(HeroService.java)

---
+++ src/main/java/cesiDI19/groupe4/services/HeroService.java
@@ -0,0 +1,24 @@
+package cesiDI19.groupe4.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import cesiDI19.groupe4.bean.Heros;
+import cesiDI19.groupe4.dao.HeroDao;
+
+@Component
+public class HeroService {
+    @Autowired
+    HeroDao herodao;
+
+    public Heros getHeros() {
+        List <Heros> listHeros = herodao.getAllHeros(0, 20);
+        if (listHeros != null && listHeros.size()>0){
+            return listHeros.get(0);
+        } else {
+            return null;
+        }
+    }
+}
